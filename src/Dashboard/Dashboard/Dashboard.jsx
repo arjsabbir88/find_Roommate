9744454fb35044b93ef9b41372f548e740b0e384
arjsabbir88 <arjsabbir88@gmail.com>
2025-06-26 08:45:30 +0600

    com

--- src/Dashboard/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import { BrowseListings } from "../../Pages/BrowseListings/BrowseListings";
 import NavBar from "../DashboardComponent/NavBar/NavBar";
 import StatsCharts from "../DashboardComponent/StartCharts/StartCharts";
 import { MyListing } from "../../Pages/My-Listing/MyListing";
-import CardContainerComponent from "../CardContainerComponent/CardContainerComponent";
+import DashboardCard from "../DashboardComponent/DashboardCard";
 
 
 
@@ -21,9 +21,40 @@ const Dashboard = () => {
       <div className="mb-8 hidden md:block">
         <NavBar></NavBar>
       </div>
-      <div>
-        <CardContainerComponent/>
-      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            <DashboardCard
+              title="Today's Money"
+              value="$53k"
+              icon={<FaCreditCard size={20} />}
+              change={55}
+              changeText="last week"
+              isPositive={true}
+            />
+            <DashboardCard
+              title="Today's Users"
+              value="2300"
+              icon={<FaUser size={20} />}
+              change={3}
+              changeText="last month"
+              isPositive={true}
+            />
+            <DashboardCard
+              title="Ads Views"
+              value="3,462"
+              icon={<FaChartBar size={20} />}
+              change={2}
+              changeText="yesterday"
+              isPositive={false}
+            />
+            <DashboardCard
+              title="Sales"
+              value="$103,430"
+              icon={<FaMoneyBillWave size={20} />}
+              change={5}
+              changeText="yesterday"
+              isPositive={true}
+            />
+          </div>
       <div className="my-8">
         <StatsCharts></StatsCharts>
       </div>
